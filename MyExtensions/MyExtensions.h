08a0b50f3e0d0214d31d94e219b7e3490ba52811
Alex Jacobson <>
2017-10-31 23:40:08 -0700

    init

---
+++ MyExtensions/MyExtensions.h
@@ -0,0 +1,19 @@
+//
+//  MyExtensions.h
+//  MyExtensions
+//
+//  Created by Alex Jacobson on 10/31/17.
+//  Copyright © 2017 Ring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyExtensions.
+FOUNDATION_EXPORT double MyExtensionsVersionNumber;
+
+//! Project version string for MyExtensions.
+FOUNDATION_EXPORT const unsigned char MyExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyExtensions/PublicHeader.h>
+
+
